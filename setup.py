86b0c35d4ac5c6f20a02447df094030fda77edf5
Shaun M <shaun@samsite.ca>
2017-05-12 10:44:59 -0700

    Update setup.py to read reqs from requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+import os
+
+# Extract requirements from requirements.txt
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requirements.txt')) as requirements_file:
+	requirements = [r.strip() for r in requirements_file if not r.startswith('#') and len(r.strip()) > 0]
 
 
 setup(
@@ -11,13 +16,5 @@ setup(
     author='dominiczippilli',
     author_email='dom@fugue.co',
     description='Python emulator for AWS Lambda.',
-    install_requires=[
-        'hurry.filesize',
-        'numpy',
-        'boto3',
-        'nose',
-        'psutil' #not strictly required by linux, but I couldn't figure out how to have per-platform builds easily
-      ],
-
-
+    install_requires=requirements
 )
